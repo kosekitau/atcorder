c2da7346e266451e6335e6eeaf9c1b24fbab3a6a
koseki <kosekitau@gmail.com>
2021-05-12 21:24:08 +0900

    zone-b

--- aaa/joi2009yo_d.cpp
+++ aaa/joi2009yo_d.cpp
@@ -1,21 +1,35 @@
 #include <iostream>
+#include <cstring>
 #include <vector>
 using namespace std;
 int dx[4]={1,0,-1,0}, dy[4]={0,-1,0,1};
 int ice[100][100];
-int m, n;
+int cice[100][100];
+int m, n, mxs=0;
 
-int dfs(int x, int y, int s){
-    int r=s+1;
-    ice[x][y]=0;
+void dfs(int x, int y, int s){
+    int r=s+1, a;
+    cice[x][y]=1;
+    //cout<<r<<' '<<x<<' '<<y<<endl;
+    /*for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cout<<cice[i][j];
+        }
+        cout<<endl;
+    }*/
+
     for(int i=0;i<4;i++){
         int nx=x+dx[i], ny=y+dy[i];
-        if(ice[nx][ny]==1 && nx>=0 && m>nx && ny>=0 && n>ny){
-            r=max(r, dfs(nx, ny, r));
+        if(ice[nx][ny]==1 && nx>=0 && m>nx && ny>=0 && n>ny && cice[nx][ny]==0){
+            //cout<<"x: "<<x<<", y: "<<y<<endl;
+            //cout<<"nx: "<<nx<<", ny: "<<ny<<endl;
+            //cout<<"i: "<<i<<endl<<endl;
+            dfs(nx, ny, r);
         }
     }
-    ice[x][y]=1;
-    return r;
+    cice[x][y]=0;
+    mxs=max(mxs, r);
+    return ;
 }
 
 int main(){
@@ -26,15 +40,17 @@ int main(){
         }
     }
     int s=0;
-
+    
     for(int i=0;i<n;i++){
         for(int j=0;j<m;j++){
-            if(ice[i][j]==1){    
-                s=max(s, dfs(i, j, 0));
+            if(ice[i][j]==1){
+                memset(cice, 0, sizeof(cice));
+                dfs(i, j, 0);
             }
         }
     }
     
-    cout<<s<<endl;
+
+    cout<<mxs<<endl;
     return 0;
 }
--- aaa/zone-b.cpp
+++ aaa/zone-b.cpp
@@ -4,19 +4,22 @@
 #include <math.h>
 using namespace std;
 using ll = long long;
+double dmax(double a, double b){
+    if(a>b) return a;
+    return b;
+}
+
 int main(){
     int N,D,H;
     cin>>N>>D>>H;
-    double max_h=0, max_d=0;
+    double min_b=0, b;
     for(int i=0;i<N;i++){
         double d, h;
         cin>>d>>h;
-        if(max_h<h){
-            max_d=d; max_h=h;
-        }
+        double k = (H-h) / (D-d);
+        b = H-k*D;
+        if(min_b<=b) min_b=b;
     }
-    double k= (H-max_h) / (D-max_d);
-    double b= H-k*D;
-    printf("%.10lf\n", b);
+    printf("%.10lf\n", dmax(0, min_b));
     return 0;
 }
