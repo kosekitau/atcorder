6e30bd2ac66941748f4e5f838829e43759199c4e
koseki <kosekitau@gmail.com>
2021-05-18 23:27:20 +0900

    201-c

--- aaa/201-c.cpp
+++ aaa/201-c.cpp
@@ -9,9 +9,25 @@ using ll = long long;
 using Graph = vector<vector<int> >;
 
 int main(){
-    int h, w;
-    cin>>h>>w;
-    
-    
+    string S;
+    cin>>S;
+
+    ll s=0;
+    for(int i=0;i<10000;i++){
+        bool flag[10];
+        memset(flag, false, sizeof(flag));
+        int x=i;
+        for(int j=0;j<4;j++){
+            flag[x%10]=true;
+            x/=10;
+        }
+        bool r=true;
+        for(int j=0;j<10;j++){
+            if(!flag[j] && S[j]=='o') r=false;
+            if(flag[j] && S[j]=='x') r=false;
+        }
+        s+=r;
+    }
+    cout<<s<<endl;
     return 0;
 }
