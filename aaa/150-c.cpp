ae32996d2f4ae7fd22a42783689998f27a0673e1
koseki <kosekitau@gmail.com>
2021-05-22 20:51:33 +0900

    150-c

--- aaa/150-c.cpp
+++ aaa/150-c.cpp
@@ -1,43 +1,27 @@
 #include <iostream>
+#include <cstdio>
 #include <vector>
 #include <algorithm>
 #include <math.h>
 using namespace std;
-
-int n, p[8],q[8];
-
-void solve(){
-    vector<int> order(n);
-    for(int i=0;i<n;i++) order[i]=i+1;
-    int k=1,a,b;
-    int count=0;
-    do {
-        count=0;
-        for(int i=0;i<n;i++){
-            if(p[i]==order[i]) count++;
-            else break;
-        }
-        if(count==n) a=k;
-
-        count=0;
-        for(int i=0;i<n;i++){
-            if(q[i]==order[i]) count++;
-            else break;
-        }
-        if(count==n) b=k;
-        k++;
-    }while(next_permutation(order.begin(), order.end()));
-    cout << abs(a-b)<<endl;
-}
 int main(){
+    int n;
+    int P[10], Q[10];
     cin>>n;
-    for(int i=0;i<n;i++){
-        cin >>p[i];
-    }
-    for(int i=0;i<n;i++){
-        cin >>q[i];
-    }
-    solve();
+    vector<int> o(n);
+    for(int i=0;i<n;i++) cin>>P[i];
+    for(int i=0;i<n;i++) cin>>Q[i];
+    for(int i=0;i<n;i++) o[i]=i+1;
     
+    int a,b,k=1;
+    do{
+        bool f1=true, f2=true;
+        for(int i=0;i<n;i++) if(P[i]!=o[i]) f1=false;
+        for(int i=0;i<n;i++) if(Q[i]!=o[i]) f2=false;
+
+        if(f1) a=k; if(f2) b=k;
+        k++;
+    }while(next_permutation(o.begin(), o.end()));
+    cout<<abs(a-b)<<endl;
     return 0;
 }
