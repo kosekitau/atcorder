e88773230b6b65ab000c8233058ffb118a9b48b6
koseki <kosekitau@gmail.com>
2021-06-14 21:14:26 +0900

    205-d

--- aaa/205-d.cpp
+++ aaa/205-d.cpp
@@ -12,17 +12,22 @@ using Graph = vector<vector<int> >;
 int main(){
     int n,q;
     cin>>n>>q;
-    ll A[100010], K[100010];
-    for(int i=1;i<=n;i++) cin>>A[i];
-    for(int i=1;i<=q;i++) cin>>K[i];
-
-    sort(K, K+q);
-    
-    ll m=0, k=1, ss=0, R[100010];
+    ll A[100010], K[100010], C[100010];
+    C[0]=0; A[0]=0;
     for(int i=1;i<=n;i++){
-        ll ss += A[i]-m+1;
-        if(ss>=K[k]){
-            R[k]=A[i]+k
+        cin>>A[i];
+        C[i]=A[i]-A[i-1]-1 + C[i-1];
+    }
+    for(int i=1;i<=q;i++) cin>>K[i];
+    
+    for(int i=1;i<=q;i++){
+        if(K[i]>C[n]){
+            cout<<K[i]-C[n]+A[n]<<endl;
+        } else {
+            //Cの中でK[i]になるとこ
+            int idx = lower_bound(C, C+n, K[i]) - C;
+            cout<<A[idx-1]+K[i]-C[idx-1]<<endl;
+            //cout<<A[idx]-(C[idx]-K[i]+1)<<endl;
         }
     }
     return 0;
