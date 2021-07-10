18721231d3ec0cdfa74e3327b076caea98abd292
koseki <kosekitau@gmail.com>
2021-07-10 11:45:14 +0900

    208-a

---
+++ aaa/208-a.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+#include <math.h>
+#include <vector>
+#include <queue>
+#include <functional>
+#include <tuple>
+#include <limits.h>
+
+using namespace std;
+using ll = long long;
+#define INF 2100000000
+
+int main(){
+    int a, b;
+    cin>>a>>b;
+    if(6*a>=b  && b>=a) cout<<"Yes"<<endl;
+    else cout<<"No"<<endl;
+    return 0;
+}
