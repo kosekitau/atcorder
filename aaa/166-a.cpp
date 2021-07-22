23cba9174760f856c9b183b5b7eec9bf0a88df73
koseki <kosekitau@gmail.com>
2021-07-22 10:11:47 +0900

    166-a

---
+++ aaa/166-a.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+#include <math.h>
+#include <vector>
+#include <queue>
+#include <functional>
+#include <tuple>
+#include<cmath>
+#include<iomanip>
+
+using namespace std;
+using ll = long long;
+typedef pair<int, int> P;
+#define INF 2100000000
+#define MOD 1000000007
+
+
+int main(){
+    string a;
+    cin>>a;
+    if(a=="ABC") cout<<"ARC"<<endl;
+    else cout<<"ABC"<<endl;
+    
+    return 0;
+}
