c8aad5e9db9152766ec61d434db1564d5c31f440
koseki <kosekitau@gmail.com>
2021-07-24 00:53:08 +0900

    165-c

--- aaa/165-c.cpp
+++ aaa/165-c.cpp
@@ -7,8 +7,8 @@
 #include <queue>
 #include <functional>
 #include <tuple>
-#include<cmath>
-#include<iomanip>
+#include <cmath>
+#include <iomanip>
 
 using namespace std;
 using ll = long long;
@@ -16,15 +16,35 @@ typedef pair<int, int> P;
 #define INF 2100000000
 #define MOD 1000000007
 
+vector<int> per;
+int a[55], b[55], c[55], d[55];
+int n, m, q, r=0;
+void dfs(int depth, int size, int mn, int mx){
+    if(depth==size){
+        int s=0;
+        //for(int i=0;i<n;i++) cout<<per[i];
+        //cout<<endl;
+        for(int i=0;i<q;i++)
+            if(per[b[i]]-per[a[i]]==c[i]) s+=d[i];
+        r=max(r, s);
+    }
+    else{
+        for(int i=mn;i<=mx;i++){
+            per[depth]=i;
+            dfs(depth+1, size, i, mx);
+        }
+    }
+}
 
 int main(){
-    ll x, i;
-    cin>>x;
-    ll s=100;
-    for(i=1;;i++){
-        s=s+s/100;
-        if(s>=x) break;
+    cin>>n>>m>>q;
+    per.resize(n);
+    for(int i=0;i<q;i++){
+        cin>>a[i]>>b[i]>>c[i]>>d[i];
+        a[i]--; b[i]--;
     }
-    cout<<i<<endl;
+    dfs(0, n, 1, m);
+    cout<<r<<endl;
+    
     return 0;
 }
