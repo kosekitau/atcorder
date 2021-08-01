49992998d216ac6a233bf55a96338c1b069a4026
koseki <kosekitau@gmail.com>
2021-08-01 22:44:21 +0900

    212-d

--- aaa/212-d.cpp
+++ aaa/212-d.cpp
@@ -15,44 +15,23 @@ using P = pair<ll, ll>;
 int main(){
     ll Q;
     cin>>Q;
-    priority_queue<P, vector<P>, greater<P>> d;
-    P que[Q+10];
-    for(int i=1;i<Q+1;i++){
+    priority_queue<ll, vector<ll>, greater<ll>> d;
+    
+    ll dx=0;
+    for(ll i=1;i<Q+1;i++){
         int q; cin>>q;
         if(q==1){
-            ll j; cin>>j;
-            que[i]=make_pair(q, j);
+            ll x; cin>>x;
+            d.push(x-dx);
         }
         else if(q==2){
             ll x; cin>>x;
-            que[i]=make_pair(q, x);
+            dx+=x;
         }
         else{
-            que[i]=make_pair(q, 0);
-        }
-    }
-
-    ll dx=0, cnt[Q+10];
-    for(int i=0;i<Q+10;i++) cnt[i]=0;
-    cnt[Q+1]=0;
-    for(int i=Q;i>0;i--){
-        cnt[i]+=cnt[i+1];
-        if(que[Q-i+1].first==2){
-            cnt[i]+=que[Q-i+1].second;
-        }
-        cout<<i<<": "<<cnt[i]<<endl;
-    }
-    for(ll i=1;i<Q+1;i++){
-        if(que[i].first==1){
-            d.push(make_pair(que[i].second, i));
-        }
-        else if(que[i].first==2){
-            continue;
-        }
-        else{
-            P z=d.top();
+            ll y=d.top();
             d.pop();
-            cout<<z.first+cnt[z.second]<<endl;
+            cout<<y+dx<<endl;
         }
     }
     return 0;
