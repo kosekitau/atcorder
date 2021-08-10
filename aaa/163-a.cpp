bc2ccac13dbba195215b71aaa64d4eff02d46457
koseki <kosekitau@gmail.com>
2021-08-11 08:24:42 +0900

    163-a

---
+++ aaa/163-a.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+#include <math.h>
+#include <vector>
+#include <queue>
+#include <functional>
+#include <map>
+#include <set>
+using namespace std;
+using ll = long long;
+using Graph = vector<vector<int> >;
+using P = pair<int, int>;
+#define INF 1000000000000001
+#define MOD 1000000007
+
+int main(void){
+    double r;
+    cin>>r;
+    cout<<2*M_PI*r<<endl;
+    return 0;
+}
