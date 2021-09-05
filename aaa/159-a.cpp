a57f4690adae756af653843200989fe581c4b44a
koseki <kosekitau@gmail.com>
2021-09-05 17:01:02 +0900

    159-a

---
+++ aaa/159-a.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+#include <math.h>
+#include <vector>
+#include <queue>
+#include <functional>
+#include <set>
+using namespace std;
+using ll = long long;
+using Graph = vector<vector<int> >;
+using P = pair<int, int>;
+#define INF 2000000003
+#define MOD 1000000007
+
+int main(){
+    int n, m;
+    cin>>n>>m;
+    cout<<n*(n-1)/2 + m*(m-1)/2<<endl;
+    return 0;
+}
