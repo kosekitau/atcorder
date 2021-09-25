f8178c1302dd2ac7272755671e5b5bf242d4b0da
koseki <kosekitau@gmail.com>
2021-09-25 11:15:43 +0900

    154-b

---
+++ aaa/154-b.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+#include <math.h>
+#include <vector>
+#include <queue>
+#include <functional>
+#include <set>
+#include <tuple>
+#include <map>
+
+using namespace std;
+using ll = long long;
+using Graph = vector<vector<int> >;
+using P = pair<int, int>;
+#define INF 2000000003
+#define MOD 1000000007
+#define MAX_N 10000000
+
+int main(){
+    string s;
+    cin>>s;
+    for(int i=0;i<s.size();i++) cout<<'x';
+    cout<<endl;
+
+    return 0;
+}
