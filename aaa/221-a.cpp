406f6a5cb74db2760c2184fdf5d2b9b2ba7816e2
koseki <kosekitau@gmail.com>
2021-10-10 09:51:35 +0900

    221-a

--- aaa/221-a.cpp
+++ aaa/221-a.cpp
@@ -15,23 +15,23 @@ using ll = long long;
 using Graph = vector<vector<int> >;
 using P = pair<int, int>;
 #define INF 2000000003
-#define MOD 998244353
-#define MAX_N 10000000
+#define MOD 100000
+#define MAX_N 90010
+
+ll modsl(ll a, ll n){
+    ll res=1;
+    while(n>0){
+        if(n & 1) res = res*a;
+        a = a*a;
+        n>>=1; 
+    }
+    return res;
+}
 
 int main(){
-    ll n, d;
-    cin>>n>>d;
-    ll lst[n+1];
-    lst[1]=1;
-    for(int i=2;i<=n;i++) lst[i]=lst[i-1]*2%MOD;
-    ll ans=0;
-    for(int i=1;i<=n;i++){
-        if(n-i>=d){
-            ans=(ans+lst[d])%MOD;
-        }
-        if(i!=1){
-            
-        }
-    }
+    int a, b;
+    cin>>a>>b;
+    if(a-b==0) cout<<1<<endl;
+    else cout<<modsl(32, a-b)<<endl;
     return 0;
 }
