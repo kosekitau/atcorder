b160535dccafeafdf7c3395a3176a5f5473adfb2
koseki <kosekitau@gmail.com>
2021-11-12 00:20:09 +0900

    153-a

---
+++ aaa/153-a.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+#include <math.h>
+#include <vector>
+#include <queue>
+#include <functional>
+#include <set>
+#include <tuple>
+#include <map>
+#include <numeric>
+
+using namespace std;
+using ll = long long;
+using Graph = vector<vector<int> >;
+using P = pair<int, int>;
+#define INF 2000000003
+#define MOD 998244353
+#define MAX_N 1010
+
+int main(){
+    int h,a;
+    cin>>h>>a;
+    if(h%a==0) cout<<h/a<<endl;
+    else cout<<h/a+1<<endl;
+    return 0;
+}
--- aaa/225-d.cpp
+++ aaa/225-d.cpp
@@ -18,88 +18,45 @@ using P = pair<int, int>;
 #define MOD 100000
 #define MAX_N 90010
 
-struct UnionFind{
-    vector<int> par, siz;
-    UnionFind(int n) : par(n, -1), siz(n, 1){ }
-
-    int root(int x){
-        if(par[x]==-1) return x;
-        else return par[x] = root(par[x]);
-    }
-
-    bool issame(int x, int y){
-        return root(x) == root(y);
-    }
-
-    bool unite(int x, int y){
-        x=root(x); y=root(y);
-        if(x==y) return false;
-        //if(siz[x]<siz[y]) swap(x, y);
-        par[y]=x;
-        siz[x]+=siz[y];
-        return true;
-    }
-
-    void reset(int x, int y){
-        par[y]=y;
-        siz[x]-=siz[y];
-        siz[y]=1;
-    }
-    void dec(int x){
-        siz[x]--;
-    }
-
-    int size(int x){
-        return siz[root(x)];
-    }
-};
-
 int main(){
     int n, q;
     cin>>n>>q;
-    UnionFind uf(n);
 
-    int lst[n];
-    for(int i=0;i<n;i++) lst[i]=i;
-    
+    int front[n];
+    int back[n];
+    for(int i=0;i<n;i++) front[i]=i;
+    for(int i=0;i<n;i++) back[i]=i;
+
     for(int i=0;i<q;i++){
         int qu; cin>>qu;
         if(qu==1){
             int x, y;
             cin>>x>>y;
             x--; y--;
-            lst[x]=y;
-            uf.unite(x, y);
+            front[y]=x;
+            back[x]=y;
         }
         else if(qu==2){
             int x, y;
             cin>>x>>y;
             x--; y--;
-            lst[x]=x;
-            uf.reset(x, y);
-            cout<<'a'<<endl;
-            int nxt=y;
-            while(1){
-                int idx=lst[nxt];
-                if(lst[idx]==idx) break;
-                uf.reset(idx, y);
-                uf.unite(y, idx);
-                nxt=idx;
-            }
+            front[y]=y;
+            back[x]=x;
         }
         else {
             int x;
             cin>>x;
             x--;
-            cout<<uf.size(x)<<' ';
-            int nxt=uf.root(x);
-            cout<<nxt<<' ';
-            while(1){
-                int idx=lst[nxt];
-                cout<<idx+1<<' ';
-                if(lst[idx]==idx) break;
-                nxt=idx;
+            int pre=x;
+            vector<int> r;
+            while(pre!=front[pre]) pre=front[pre];
+            r.push_back(pre);
+            while(pre!=back[pre]) {
+                pre=back[pre];
+                r.push_back(pre);
             }
+            cout<<r.size()<<' ';
+            for(auto nxt: r) cout<<nxt+1<<' ';
             cout<<endl;
         }
     }
