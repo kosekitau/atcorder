e8c13ef3b45a51aadc1934361c068197f6101394
koseki <kosekitau@gmail.com>
2021-12-03 22:52:34 +0900

    144-a

---
+++ aaa/144-a.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+#include <math.h>
+#include <vector>
+#include <queue>
+#include <functional>
+#include <set>
+#include <tuple>
+#include <map>
+#include <numeric>
+
+using namespace std;
+using ll = long long;
+using Graph = vector<vector<int> >;
+using P = pair<int, int>;
+#define INF 2000000003
+#define MOD 998244353
+#define MAX_N 1010
+
+int main(){
+    int k, t;
+    cin>>k>>t;
+    if(k>9 || t>9) cout<<-1<<endl;
+    else cout<<k*t<<endl;
+    
+    return 0;
+}
