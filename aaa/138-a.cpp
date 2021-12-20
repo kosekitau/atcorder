8aa1355fe5b97c7fd46451610ee2a4601621bfdf
koseki <kosekitau@gmail.com>
2021-12-20 22:15:08 +0900

    138-a

---
+++ aaa/138-a.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+#include <math.h>
+#include <vector>
+#include <queue>
+#include <functional>
+#include <set>
+#include <tuple>
+#include <map>
+#include <numeric>
+
+using namespace std;
+using ll = long long;
+using Graph = vector<vector<ll> >;
+using P = pair<string, string>;
+#define INF 2000000003
+#define MOD 998244353
+#define MAX_N 1010
+
+int main(){
+    int n;
+    string s;
+    cin>>n; cin>>s;
+    if(n>=3200) cout<<s<<endl;
+    else cout<<"red"<<endl;
+    return 0;
+}
